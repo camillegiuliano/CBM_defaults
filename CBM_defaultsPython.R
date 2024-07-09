1ea5bbc9bef05e6603b9cf506a2b8eead4f78ffb
camillegiuliano <camille.giuliano@nrcan-rncan.gc.ca>
2024-07-09 15:41:02 -0700

    creating empty module script

---
+++ CBM_defaultsPython.R
@@ -0,0 +1,131 @@
+defineModule(sim, list(
+  name = "CBM_defaultsPython",
+  description = "Reads in all the default values for CBM-CFS3 for Canada",
+  keywords = "",
+  authors = ,
+  version = list(CBM_defaultsPython = "0.1"),
+  timeframe = as.POSIXlt(c(NA, NA)),
+  timeunit = "year",
+  citation = list("citation.bib"),
+  documentation = list("README.txt", "CBM_defaults.Rmd"),
+  reqdPkgs = list("SpaDES.core (>= 2.1.5.9000)", "ggplot2", "RSQLite", "CBMutils"),
+
+  parameters = bindrows(
+    #defineParameter("paramName", "paramClass", value, min, max, "parameter description"),
+    defineParameter(".plots", "character", "screen", NA, NA,
+                    "Used by Plots function, which can be optionally used here"),
+    defineParameter(".plotInitialTime", "numeric", start(sim), NA, NA,
+                    "Describes the simulation time at which the first plot event should occur."),
+    defineParameter(".plotInterval", "numeric", NA, NA, NA,
+                    "Describes the simulation time interval between plot events."),
+    defineParameter(".saveInitialTime", "numeric", NA, NA, NA,
+                    "Describes the simulation time at which the first save event should occur."),
+    defineParameter(".saveInterval", "numeric", NA, NA, NA,
+                    "This describes the simulation time interval between save events."),
+    defineParameter(".studyAreaName", "character", NA, NA, NA,
+                    "Human-readable name for the study area used - e.g., a hash of the study",
+                          "area obtained using `reproducible::studyAreaName()`"),
+    ## .seed is optional: `list('init' = 123)` will `set.seed(123)` for the `init` event only.
+    defineParameter(".seed", "list", list(), NA, NA,
+                    "Named list of seeds to use for each event (names)."),
+    defineParameter(".useCache", "logical", FALSE, NA, NA,
+                    "Should caching of events or module be used?")
+  ),
+
+  inputObjects = bindrows(
+    #expectsInput("objectName", "objectClass", "input object description", sourceURL, ...),
+    expectsInput(objectName = NA, objectClass = NA, desc = NA, sourceURL = NA)
+  ),
+  outputObjects = bindrows(
+    #createsOutput("objectName", "objectClass", "output object description", ...),
+    createsOutput(objectName = NA, objectClass = NA, desc = NA)
+  )
+))
+
+
+doEvent.CBM_defaultsPython.init <- function(sim, eventTime, eventType, priority) {
+
+  return(sim)
+}
+
+### template initialization
+Init <- function(sim) {
+  # # ! ----- EDIT BELOW ----- ! #
+
+  # ! ----- STOP EDITING ----- ! #
+
+  return(invisible(sim))
+}
+### template for save events
+Save <- function(sim) {
+  # ! ----- EDIT BELOW ----- ! #
+  # do stuff for this event
+  sim <- saveFiles(sim)
+
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+
+### template for plot events
+plotFun <- function(sim) {
+  # ! ----- EDIT BELOW ----- ! #
+  # do stuff for this event
+  sampleData <- data.frame("TheSample" = sample(1:10, replace = TRUE))
+  Plots(sampleData, fn = ggplotFn) # needs ggplot2
+
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+
+### template for your event1
+Event1 <- function(sim) {
+  # ! ----- EDIT BELOW ----- ! #
+  # THE NEXT TWO LINES ARE FOR DUMMY UNIT TESTS; CHANGE OR DELETE THEM.
+  # sim$event1Test1 <- " this is test for event 1. " # for dummy unit test
+  # sim$event1Test2 <- 999 # for dummy unit test
+
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+
+### template for your event2
+Event2 <- function(sim) {
+  # ! ----- EDIT BELOW ----- ! #
+  # THE NEXT TWO LINES ARE FOR DUMMY UNIT TESTS; CHANGE OR DELETE THEM.
+  # sim$event2Test1 <- " this is test for event 2. " # for dummy unit test
+  # sim$event2Test2 <- 777  # for dummy unit test
+
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+
+.inputObjects <- function(sim) {
+  # Any code written here will be run during the simInit for the purpose of creating
+  # any objects required by this module and identified in the inputObjects element of defineModule.
+  # This is useful if there is something required before simulation to produce the module
+  # object dependencies, including such things as downloading default datasets, e.g.,
+  # downloadData("LCC2005", modulePath(sim)).
+  # Nothing should be created here that does not create a named object in inputObjects.
+  # Any other initiation procedures should be put in "init" eventType of the doEvent function.
+  # Note: the module developer can check if an object is 'suppliedElsewhere' to
+  # selectively skip unnecessary steps because the user has provided those inputObjects in the
+  # simInit call, or another module will supply or has supplied it. e.g.,
+  # if (!suppliedElsewhere('defaultColor', sim)) {
+  #   sim$map <- Cache(prepInputs, extractURL('map')) # download, extract, load file from url in sourceURL
+  # }
+
+  #cacheTags <- c(currentModule(sim), "function:.inputObjects") ## uncomment this if Cache is being used
+  dPath <- asPath(getOption("reproducible.destinationPath", dataPath(sim)), 1)
+  message(currentModule(sim), ": using dataPath '", dPath, "'.")
+
+  # ! ----- EDIT BELOW ----- ! #
+
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+
+ggplotFn <- function(data, ...) {
+  ggplot2::ggplot(data, ggplot2::aes(TheSample)) +
+    ggplot2::geom_histogram(...)
+}
+
