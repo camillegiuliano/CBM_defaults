44879e6d8263a74f298b3d83beb1ae794d7d6af9
camillegiuliano <camille.giuliano@nrcan-rncan.gc.ca>
2024-07-31 08:18:35 -0700

    getting rid of cbmdefaultsPython subfolder and replacing old cbm_default

--- CBM_defaults.R
+++ CBM_defaults.R
@@ -12,44 +12,38 @@ defineModule(sim, list(
   timeunit = "year",
   citation = list("citation.bib"),
   documentation = list("README.txt", "CBM_defaults.Rmd"),
-  reqdPkgs = list("RSQLite", "data.table",
-                  "PredictiveEcology/CBMutils"),
-  parameters = rbind(
-    # defineParameter("paramName", "paramClass", value, min, max, "parameter description"),
-    defineParameter(".plotInitialTime", "numeric", NA, NA, NA,
-                    "This describes the simulation time at which the first plot event should occur"),
+  reqdPkgs = list("RSQLite", "data.table"
+                  ##TODO: get this message currently when adding CBMUtils: CBMutils not on CRAN; checking CRAN archives ...
+  ),
+
+  parameters = bindrows( ##TODO: these are all default SpaDES parameters, not sure if all are needed here
+    #defineParameter("paramName", "paramClass", value, min, max, "parameter description"),
+    defineParameter(".plotInitialTime", "numeric", start(sim), NA, NA,
+                    "Describes the simulation time at which the first plot event should occur."),
     defineParameter(".plotInterval", "numeric", NA, NA, NA,
-                    "This describes the simulation time interval between plot events"),
+                    "Describes the simulation time interval between plot events."),
     defineParameter(".saveInitialTime", "numeric", NA, NA, NA,
-                    "This describes the simulation time at which the first save event should occur"),
+                    "Describes the simulation time at which the first save event should occur."),
     defineParameter(".saveInterval", "numeric", NA, NA, NA,
-                    "This describes the simulation time interval between save events"),
+                    "This describes the simulation time interval between save events."),
     defineParameter(".useCache", "logical", FALSE, NA, NA,
-                    paste("Should this entire module be run with caching activated?",
-                          "This is generally intended for data-type modules,",
-                          "where stochasticity and time are not relevant."))
+                    "Should caching of events or module be used?") ##TODO: keep if caching
   ),
-  inputObjects = bindrows(
-    expectsInput(objectName = "sqlDir", objectClass = "character", desc = NA, sourceURL = NA),
-    expectsInput(objectName = "dbPath", objectClass = "character", desc = NA, sourceURL = NA)
+
+  inputObjects = bindrows( ##TODO: find inputs (so far just dbPath)
+    expectsInput(objectName = "dbPath", objectClass = "character", desc = NA, sourceURL = NA),
+
   ),
-  outputObjects = bindrows(
-    createsOutput(objectName = "cbmData", objectClass = "dataset",
-                  desc = NA), ## TODO
-    createsOutput(objectName = "decayRates", objectClass = "matrix",
-                  desc = "decay rates per spatial unit?"),
-    createsOutput(objectName = "PoolCount", objectClass = "numeric",
-                  desc = "Length of pooldef"),
-    createsOutput(objectName = "pooldef", objectClass = "character",
-                  desc = "Vector of names (characters) for each of the carbon pools, with `Input` being the first one"),
-    createsOutput(objectName = "processes", objectClass = "list",
-                  desc = "decay mixing turnover and disturbances")
+  outputObjects = bindrows( ##TODO: find outputs, cbmdata, disturbance data, species data
+    #createsOutput("objectName", "objectClass", "output object description", ...),
+    createsOutput(objectName = "cbmData", objectClass = "dataset", desc = NA),
+    createsOutput(objectName = "pooldef", objectClass = "character", desc = NA),
+    createsOutput(objectName = "poolCount", objectClass = "numeric", desc = NA),
+    createsOutput(objectName = "", objectClass = "", desc = NA), ##TODO: add missing outputs
   )
 ))
 
-## event types
-#   - type `init` is required for initialiazation
-
+##TODO this is copied from current CBM_defaults, unsure if that needs to change? it's still only 1 event
 doEvent.CBM_defaults <- function(sim, eventTime, eventType, debug = FALSE) {
   switch(
     eventType,
@@ -77,104 +71,58 @@ doEvent.CBM_defaults <- function(sim, eventTime, eventType, debug = FALSE) {
   )
   return(invisible(sim))
 }
-
-## event functions
-#   - follow the naming convention `modulenameEventtype()`;
-#   - `modulenameInit()` function is required for initiliazation;
-#   - keep event functions short and clean, modularize by calling subroutines from section below.
-
 ### template initialization
 Init <- function(sim) {
   # # ! ----- EDIT BELOW ----- ! #
-  sim$pooldef <- CBMutils::.pooldef
-  sim$PoolCount <- length(sim$pooldef)
+  #extract data from database
+  archiveIndex <- dbConnect(dbDriver("SQLite"), sim$dbPath)
 
-  # step 1 read the cbm_defaults parameter data
-  spatialUnitIds <- as.matrix(getTable("spatialUnitIds.sql", sim$dbPath, sim$sqlDir))
-  disturbanceMatrix <- as.matrix(getTable("disturbanceMatrix.sql", sim$dbPath, sim$sqlDir))
-  # this is the S4 object that has ALL the parameters
-  sim$cbmData <- new("dataset",
-    turnoverRates = as.matrix(getTable("turnoverRates.sql", sim$dbPath, sim$sqlDir)),
-    rootParameters = as.matrix(getTable("rootParameters.sql", sim$dbPath, sim$sqlDir)),
-    decayParameters = as.matrix(getTable("decayParameters.sql", sim$dbPath, sim$sqlDir)),
-    spinupParameters = as.matrix(getTable("spinupParameters.sql", sim$dbPath, sim$sqlDir)),
-    climate = as.matrix(getTable("climate.sql", sim$dbPath, sim$sqlDir)),
-    spatialUnitIds = spatialUnitIds,
-    slowAGtoBGTransferRate = as.matrix(0.006),
-    biomassToCarbonRate = as.matrix(0.5),
-    stumpParameters = as.matrix(getTable("stumpParameters.sql", sim$dbPath, sim$sqlDir)),
-    overmatureDeclineParameters = as.matrix(getTable("overmaturedecline.sql", sim$dbPath, sim$sqlDir)),
-    disturbanceMatrix = disturbanceMatrix,
-    disturbanceMatrixAssociation = as.matrix(getTable("disturbanceMatrixAssociation.sql", sim$dbPath, sim$sqlDir)),
-    disturbanceMatrixValues = as.matrix(getTable("disturbanceMatrixValues.sql", sim$dbPath, sim$sqlDir)),
-    landclasses = as.matrix(getTable("landclasses.sql", sim$dbPath, sim$sqlDir)),
-    pools = as.matrix(getTable("pools.sql", sim$dbPath, sim$sqlDir)),
-    domPools = as.matrix(getTable("domPools.sql", sim$dbPath, sim$sqlDir))
-  )
+  #extract disturbance tables
+  disturbanceMatrix <- as.data.table(dbGetQuery(archiveIndex, "SELECT * FROM disturbance_matrix"))
+  sim$disturbanceMatrixAssociation <- as.data.table(dbGetQuery(archiveIndex, "SELECT * FROM disturbance_matrix_association")) #matrices2
+  sim$disturbanceMatrixTr <- as.data.table(dbGetQuery(archiveIndex, "SELECT * FROM disturbance_matrix_tr")) #matrices 3
+  disturbanceMatrixValue <- as.data.table(dbGetQuery(archiveIndex, "SELECT * FROM disturbance_matrix_value"))
+  sim$disturbanceType <- as.data.table(dbGetQuery(archiveIndex, "SELECT * FROM disturbance_type"))
+  disturbanceTypeTr <- as.data.table(dbGetQuery(archiveIndex, "SELECT * FROM disturbance_type_tr"))
+  #linking disturbance tables
+  disturbanceTypeTable <- disturbanceMatrixAssociation[disturbanceTypeTr, on = .(disturbance_type_id = disturbance_type_id), allow.cartesian = TRUE]
+  disturbanceMatrixTable <- disturbanceMatrixValue[disturbanceMatrixTr, on = .(disturbance_matrix_id = disturbance_matrix_id), allow.cartesian = TRUE]
+  disturbanceMatrixLink <- disturbanceMatrixTable[disturbanceTypeTable, on = .(disturbance_matrix_id = disturbance_matrix_id), allow.cartesian = TRUE]
+  ##TODO: this last one is HUGE (>3 million rows)
 
-  # step 2 create constant matrices from parameter data
-  # these are the decay rates for all the 48 spatial units
-  sim$decayRates <- spatialUnitDecayRates(sim$cbmData@climate, sim$cbmData@decayParameters, sim$cbmData@domPools)
+  #extract spinup and spatial unit ID tables
+  spatialUnitIds <- as.data.table(dbGetQuery(archiveIndex, "SELECT * FROM spatial_unit")) ##TODO: do we want this in spinupSQL or just the paramater file?
+  spinupParameter <-  as.data.table(dbGetQuery(archiveIndex, "SELECT * FROM spinup_parameter"))
+  #linking spinup and spatial IDs
+  sim$spinupSQL <- spatialUnitIds[spinupParameter, on = .(spinup_parameter_id = id)]
 
-  sim$processes <- list(
-    domDecayMatrices = matrixHash(computeDomDecayMatrices(sim$decayRates, sim$cbmData@decayParameters, sim$PoolCount)),
-    slowDecayMatrices = matrixHash(computeSlowDecayMatrices(sim$decayRates, sim$cbmData@decayParameters, sim$PoolCount)),
-    slowMixingMatrix = matrixHash(computeSlowMixingMatrix(sim$cbmData@slowAGtoBGTransferRate, sim$PoolCount)),
-    domTurnover = matrixHash(computeDomTurnoverMatrices(sim$cbmData@turnoverRates, sim$PoolCount)),
-    bioTurnover = matrixHash(computeBioTurnoverMatrices(sim$cbmData@turnoverRates, sim$PoolCount)),
-    disturbanceMatrices = matrixHash(loadDisturbanceMatrixIds(sim$cbmData@disturbanceMatrixValues, sim$cbmData@pools))
-  )
-## assertion to check if annual process proportional matrices all have a sumed value of 1 per row.
-  propTransfer <- NULL
-  for (i in setdiff(names(sim$processes), "disturbanceMatrices")) {
-    makeDT <- matrixDT(matricesIn = sim$processes[[i]], indicesIn = names(sim$processes[[i]]))
-    propCheck <- checkProp(makeDT)
-    if (sum(propCheck$noLoss) != length(propCheck$noLoss))
-      stop("Transfer matrices have proportions different then 1: carbon is disappearing or appearing")
-  }
+  #extract for pooldef
+  sim$pooldef <- as.data.table(dbGetQuery(archiveIndex, "SELECT * FROM pool"))
+  poolTR <- as.data.table(dbGetQuery(archiveIndex, "SELECT * FROM pool_tr")) ##maybe not needed
+
+  #find forest_type_id
+  sim$forestTypeId <- as.data.table(dbGetQuery(archiveIndex, "SELECT * FROM forest_type_tr"))
+
+
+  ##TODO: eventually figure what needs to be extracted from database
 
   # ! ----- STOP EDITING ----- ! #
 
-  return(invisible(sim))
-}
-
-### template for save events
-Save <- function(sim) {
-  # ! ----- EDIT BELOW ----- ! #
-  # do stuff for this event
-  sim <- saveFiles(sim)
-
-  # ! ----- STOP EDITING ----- ! #
-  return(invisible(sim))
-}
-
-### template for plot events
-plot <- function(sim) {
-  # ! ----- EDIT BELOW ----- ! #
-  # do stuff for this event
-  # Plot("object")
-
-  # ! ----- STOP EDITING ----- ! #
   return(invisible(sim))
 }
 
 .inputObjects <- function(sim) {
-  cacheTags <- c(currentModule(sim), "function:.inputObjects")
-  dPath <- asPath(getOption("reproducible.destinationPath", dataPath(sim)), 1)
-  message(currentModule(sim), ": using dataPath '", dPath, "'.")
 
-  if (dPath != dataPath(sim)) {
-    checkPath(file.path(dPath, "cbm_defaults"), create = TRUE)
-    file.copy(file.path(dataPath(sim), "cbm_defaults"), dPath, recursive = TRUE, overwrite = TRUE)
-  }
+  #cacheTags <- c(currentModule(sim), "function:.inputObjects") ## uncomment this if Cache is being used
+  dPath <- asPath(getOption("reproducible.destinationPath", dataPath(sim)), 1)
+  message(currentModule(sim), ": using dataPath '", dPath, "'.") ##TODO: figure out what this does/means, and what needs to be changed for this module
 
   # ! ----- EDIT BELOW ----- ! #
-  if (!suppliedElsewhere(sim$sqlDir)) {
-    sim$sqlDir <- file.path(dPath, "cbm_defaults")
-  }
 
   if (!suppliedElsewhere(sim$dbPath)) {
-    sim$dbPath <- file.path(sim$sqlDir, "cbm_defaults.db")
+    sim$dbPath <- "C:/Camille/GitHub/spadesCBM/defaultDB/cbm_defaults_v1.2.8340.362.db"
+    ##TODO: this eventually needs to not lead to a locally stored file
+    ## download file here: https://github.com/cat-cfs/libcbm_py/tree/main/libcbm/resources/cbm_defaults_db
   }
 
   # ! ----- STOP EDITING ----- ! #
